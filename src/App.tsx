a1dcf15862f8a21f8e0e802e9dd0ee452f80db1b
Nguyen Nhat Thanh <nhatthanhlep10@gmail.com>
2022-11-27 05:13:32 +0700

    last of last commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ResolverReportDetail from 'pages/ResolverReportDetail';
 import ResolverReports from 'pages/ResolverReports';
 import ReportDetailOfReportor from 'pages/ReportDetailOfReportor';
 import { Route, Routes } from 'react-router-dom';
+import Login from 'pages/Login';
 import Reportor from './pages/Reportor';
 import Top10Users from 'pages/Top10Users/Top10Users';
 function App() {
@@ -14,9 +15,7 @@ function App() {
             <Route path='/' element={<Map />} />
             {/* <Route path="/resolver/report" element={<ResolverReportDetail />} /> */}
 
-            <Route path='/resolver/report' element={<ResolverReports />} />
-            <Route path='/reportor/top' element={<Top10Users />} />
-            <Route path='/resolver/top' element={<Top10Users />} />
+            <Route path='/resolver/report' element={<ResolverReportDetail />} />
         </Routes>
     );
 }
